a5e0736781476a4e2dc74ed2007b5690bbca30c4
Yohanan Assefa <nanahoy@gmail.com>
2017-10-30 11:56:12 -0600

    Add project page

--- server.js
+++ server.js
@@ -45,9 +45,15 @@ app.get('/', (req, res) => {
 
 app.get('/about', (req, res) => {
   res.render('about.hbs', {
-    pageTitle: 'About Page',
+    pageTitle: 'About Page'
   });
 });
+
+app.get('/projects', (req, res) => {
+  res.render('projects.hbs', {
+    pageTitle: 'Projects Page'
+  });
+})
 
 app.get('/bad', (req, res) => {
   res.send({
